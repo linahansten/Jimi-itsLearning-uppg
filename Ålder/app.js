3f0d8c13768fcd43edde1bee7e01cc7e8f6ef895
linahansten <skolkontolina@gmail.com>
2023-05-03 08:57:09 +0300

    init

--- Ålder/app.js
+++ Ålder/app.js
@@ -1,7 +1,8 @@
-const inputElement = document.querySelector('input');
-const divElement = document.querySelector('div');
+const inputElement = document.querySelector("input");
+const divElement = document.querySelector("div");
+const pElement = document.querySelector("p")
 
-inputElement.addEventListener('change', () => {
+inputElement.addEventListener("change", () => {
     const result = computeAge(inputElement.value);
     divElement.innerHTML = `
     Du är ${result.years} år och ${result.days} dagar gammal
@@ -32,4 +33,18 @@ inputElement.addEventListener('change', () => {
             }
         }
     }
+
+    if (result.years < 0) {
+        pElement.innerText = "Ange en gilitg ålder"
+    } else if (result.years < 12) {
+        pElement.innerText = "Du är ett barn"
+    } else if (result.years >= 13 && result.years <= 19) {
+        pElement.innerText = "Du är en tonåring"
+    } else if (result.years >= 20 && result.years <= 64) {
+        pElement.innerText = "Du är en vuxen"
+    } else if (result.years >= 65 && result.years <= 1000) {
+        pElement.innerText = "Du är en senior"
+    }
 })
+
+
