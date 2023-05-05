54306ea380102806183248dab71d0307678b8998
linahansten <skolkontolina@gmail.com>
2023-05-05 10:44:31 +0300

    init

--- spara lokalt WIP/app.js
+++ spara lokalt WIP/app.js
@@ -1,23 +1,24 @@
+
+console.log(localStorage.getItem("lang"))
+
+document.querySelectorAll("#myDropdown a").forEach(el => {
+    el.addEventListener("click", select)
+
+})
+
 function dropdown() {
     document.getElementById("myDropdown").classList.toggle("show")
 }
 
-function select() {
-    console.log(event.target.textContent)
-    const dropdowns = document.querySelectorAll(".dropdown-content a")
-    for (let i = 0; i < dropdowns.length; i++) {
-        let openDropdown = dropdowns[i]
-        if (openDropdown.textContent == "svenska") {
-            console.log("svenska")
-        } else if (openDropdown.textContent == "suomi") {
-            console.log("suomi")
-        } else if (openDropdown.textContent == "english") {
-            console.log("english")
-        }
-
-        if (openDropdown.classList.contains("show")) {
-            openDropdown.classList.remove("show")
-        }
-    }
+function select(e) {
+    localStorage.setItem("lang", e.target.textContent)
+    document.getElementById("myDropdown").classList.toggle("show")
+    window.location.reload()
 }
+function text() {
+    const p = document.querySelector(".lang")
+    p.textContent = "your language is " + localStorage.getItem("lang")
+
+}
+text()
 
